0355e48eb75190668093fa4d29a5dcfbb523f9e1
Sanjay Baitha <91378949+rajakprogrammer@users.noreply.github.com>
2021-09-26 05:29:07 -0700

    Add files via upload

---
+++ playing with character.c
@@ -0,0 +1,11 @@
+ char c,s[50],sen[100];
+    
+    
+   scanf("%c",&c);
+   scanf("%s\n",&s);
+   scanf("%[^\n]s",&sen);
+   
+   printf("%c",c);
+   printf("\n%s",s);
+   printf("\n%s",sen);
+        
